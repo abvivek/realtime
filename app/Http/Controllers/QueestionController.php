cb48c6fb205e1b21966934302b5d8173082de7d2
abvivek <abvivek1@gmail.com>
2020-07-18 20:01:22 +0530

    after migration, model, ctrls

---
+++ app/Http/Controllers/QueestionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Queestion;
+use Illuminate\Http\Request;
+
+class QueestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Queestion  $queestion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Queestion $queestion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Queestion  $queestion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Queestion $queestion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Queestion  $queestion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Queestion $queestion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Queestion  $queestion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Queestion $queestion)
+    {
+        //
+    }
+}
---
+++ database/factories/QueestionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Queestion;
+use Faker\Generator as Faker;
+
+$factory->define(Queestion::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

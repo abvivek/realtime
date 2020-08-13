2c2d4d77f0e143164d33612344e6943de229f8da
abvivek <abvivek1@gmail.com>
2020-08-13 12:46:19 +0530

    Like and dislike for reply

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -3,83 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Likes;
+use App\Reply;
+
 use Illuminate\Http\Request;
 
 class LikesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function likeIt(Reply $reply){
+        $reply->like()->create(
+            // 'user_id' => auth()->id
+            'user_id' => '1',
+        );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function unLikeIt(Reply $reply){
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Likes  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Likes $likes)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Likes  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Likes $likes)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Likes  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Likes $likes)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Likes  $likes
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Likes $likes)
-    {
-        //
+        // $reply->like()->where(['user_id', auth()->id()])->first()->delete();
+        $reply->like()->where('user_id', '1')->first()->delete();
+        
     }
 }
